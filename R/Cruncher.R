752add274b1118ef64398168bc7776a9637a6737
Anna SMYK <anna.smyk@insee.fr>
2026-04-15 13:28:28 +0200

    up date

--- R/Cruncher.R
+++ R/Cruncher.R
@@ -1,8 +1,10 @@
 #install.packages("rjwsacruncher")
 library(rjwsacruncher)
-#browseVignettes("rjwsacruncher")
-options(cruncher_bin_directory = "C:/Software/jwsacruncher-3.3.0/jwsacruncher-3.3.0/bin")
+browseVignettes("rjwsacruncher")
+options(cruncher_bin_directory = "C:/INSEE/LogicielsPortables/jwsacruncher-standalone-3.7.1-windows-x86_64/jwsacruncher-3.7.1/bin")
 getOption("cruncher_bin_directory")
+options(is_cruncher_v3 = TRUE)
+getOption("is_cruncher_v3")
 options(default_tsmatrix_series = c("y","sa","s","d18","ycal","t","i"))
 
 
@@ -13,6 +15,9 @@ cruncher_and_param(workspace = "Workspaces/Ipi_test.xml", #target: workspace's m
                    csv_layout = "vtable" # vertical layout
                    ,log_file = "ipi_log.txt" #log file
                    )
+
+## see workspace file and log
+## see JD+ documentation / Tools / Cruncher 
 
 
 ### Production of a quality report possible : use JDCruncher package
